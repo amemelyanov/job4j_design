8bd01253403f88056084c717b64fb0979f92e64b
AMEMELYANOV <emelyanov.am@gmail.com>
2021-04-11 11:09:04 +0300

    0.4. BufferedOutputStream [#504105]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +16,21 @@ public class LogFilter {
         return lines;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)))) {
+            for (String line : log) {
+                out.printf("%s%n", line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         log.forEach(System.out::println);
+        save(log, "404.txt");
     }
 }
