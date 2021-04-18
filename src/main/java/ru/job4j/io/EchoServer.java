b48beb8bce6436ac281b691354a183da93b39ac0
AMEMELYANOV <emelyanov.am@gmail.com>
2021-04-18 20:46:26 +0300

    1. Бот [#504126]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -15,12 +15,23 @@ public class EchoServer {
                     String str;
                     while (in.ready()) {
                         str = in.readLine();
-                        if (str.contains("=") && str.split("=")[1].startsWith("Bye")) {
-                            server.close();
+                        String textInput;
+                        if (str.contains("msg=")) {
+                            textInput = str.split(" ")[1].split("=")[1];
+                            if (textInput.equals("Exit")) {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("Завершить работу сервера.".getBytes("WINDOWS-1251"));
+                                server.close();
+                            } else if (textInput.equals("Hello")) {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write("Hello.".getBytes());
+                            } else {
+                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                out.write(textInput.getBytes());
+                            }
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 }
             }
         }
