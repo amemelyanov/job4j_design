4c9cc68fad32a1974829959c6dcc05cec41e6e96
AMEMELYANOV <emelyanov.am@gmail.com>
2021-05-26 17:18:46 +0300

    2. Что такое TDD? [#4918 #160257]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -46,4 +46,41 @@ public class CinemaTest {
         List<Session> sessions = cinema.find(session -> true);
         assertEquals(sessions.size(), 2);
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void doubleTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalPlaceTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, -1, -1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalDateTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void illegalDateAndPlaceTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1900, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, -1, 1, date);
+    }
 }
